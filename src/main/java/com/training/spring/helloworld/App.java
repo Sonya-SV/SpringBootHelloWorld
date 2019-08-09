d796c1865872848ab75a304ee9612d606206807e
Sonya <sofi.svyrydenko@gmail.com>
2019-08-09 13:32:17 +0300

    Initial commit

---
+++ src/main/java/com/training/spring/helloworld/App.java
@@ -0,0 +1,22 @@
+package com.training.spring.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(App.class, args);
+    }
+    @RequestMapping("/hello")
+    public String showHelloWorld(){
+        return "hello-world";
+    }
+}
